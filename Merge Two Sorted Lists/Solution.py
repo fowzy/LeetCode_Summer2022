4e9f3d6a1c532cf18a2476bb68150b6f2f883445
fowzy <fwzsas@gmail.com>
2022-07-12 09:31:36 -0500

    Start working on Merge Two Sorted List problem.

--- Merge Two Sorted Lists/Solution.py
+++ Merge Two Sorted Lists/Solution.py
@@ -1,22 +1,20 @@
 # Definition for singly-linked list.
 from typing import List
 
-
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        n = ListNode()
-        newList = []
-        # newList = sorted(list1+list2)
-        return newList
-
+        pass
 c0 = Solution()
 print(c0.mergeTwoLists([1,2,4],[1,3,4]))
+# should return [1,1,2,3,4,4]
 c1 = Solution()
 print(c1.mergeTwoLists([],[]))
+# should return []
 c2 = Solution()
 print(c2.mergeTwoLists([],[0]))
+# should return [0]
 
